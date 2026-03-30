fdb2790b74a9e574c5316e126b07c8f0f81b204d
Edith Heidmann <eheidmann@gmail.com>
2026-03-30 13:15:19 +0200

    initial commit, tailwind, vite config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1 className="bg-red-200">This will be sesh love</h1>
+      </section>
+    </>
+  );
+}
+
+export default App;
