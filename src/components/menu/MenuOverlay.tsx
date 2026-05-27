a64d7ea88c138712bbc05316e70da9be597d9934
Edith Heidmann <eheidmann@gmail.com>
2026-05-27 14:16:34 +0200

    setup nav menu overlay

---
+++ src/components/menu/MenuOverlay.tsx
@@ -0,0 +1,58 @@
+type Props = {
+  navOpen: boolean;
+  onNavClose: () => void;
+};
+
+const MenuOverlay = ({ navOpen, onNavClose }: Props) => {
+  return (
+    <div
+      className={`
+        fixed inset-0 z-50
+        transition-all duration-200
+        ${navOpen ? "pointer-events-auto" : "pointer-events-none"}
+      `}
+    >
+      {/* Backdrop */}
+      <div
+        onClick={onNavClose}
+        className={`
+          absolute inset-0
+          bg-black/40
+          transition-opacity duration-200
+          ${navOpen ? "opacity-100" : "opacity-0"}
+        `}
+      />
+
+      {/* Panel */}
+      <aside
+        className={`
+          absolute top-0 right-0
+          h-full w-full lg:w-1/2
+
+          bg-(--color-primary)
+
+          overflow-hidden
+          will-change-transform
+
+          transition-transform duration-200 ease-out
+
+          ${navOpen ? "translate-x-0" : "translate-x-[101%]"}
+        `}
+      >
+        <div className="flex justify-end p-6">
+          <button onClick={onNavClose} className="focus:outline-none">
+            close
+          </button>
+        </div>
+
+        <nav className="flex flex-col gap-6 p-6 text-lg text-white">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+        </nav>
+      </aside>
+    </div>
+  );
+};
+
+export default MenuOverlay;
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import MenuOverlay from "../menu/MenuOverlay";
+
 const NavBar = () => {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <nav className="w-full flex items-center justify-between px-4 py-4 md:py-6">
-      <div>logo / contact (conditionally depending on view)</div>
-      <div>'menu' for dropdown menu</div>
+      {/* // todo: if on home page 'contact' else 'logo for home' */}
+      {/* // todo: decide on transition of hover and hover colour / opacity */}
+      <button className="text-3xl hover:opacity-70 transition">contact</button>
+
+      <button
+        className="text-3xl hover:opacity-70 transition"
+        onClick={() => setNavOpen(true)}
+      >
+        menu
+      </button>
+
+      <MenuOverlay navOpen={navOpen} onNavClose={() => setNavOpen(false)} />
     </nav>
   );
 };
