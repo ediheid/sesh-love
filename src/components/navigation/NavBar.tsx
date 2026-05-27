14108f67abc27bfa0a53053637d33d5a6503391c
Edith Heidmann <eheidmann@gmail.com>
2026-05-27 14:48:17 +0200

    adjust font and layout for responsivity

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -4,13 +4,15 @@ import MenuOverlay from "../menu/MenuOverlay";
 const NavBar = () => {
   const [navOpen, setNavOpen] = useState(false);
   return (
-    <nav className="w-full flex items-center justify-between px-4 py-4 md:py-6">
+    <nav className="w-full flex items-center justify-between px-4 py-4 md:py-6 md:px-6">
       {/* // todo: if on home page 'contact' else 'logo for home' */}
       {/* // todo: decide on transition of hover and hover colour / opacity */}
-      <button className="text-3xl hover:opacity-70 transition">contact</button>
+      <button className="text-2xl md:text-3xl hover:opacity-70 transition">
+        contact
+      </button>
 
       <button
-        className="text-3xl hover:opacity-70 transition"
+        className="text-2xl md:text-3xl hover:opacity-70 transition"
         onClick={() => setNavOpen(true)}
       >
         menu
