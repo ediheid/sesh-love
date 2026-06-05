5520116318f3a7e967329951c65bdec66fef05c4
Edith Heidmann <eheidmann@gmail.com>
2026-06-05 14:12:19 +0200

    add navigation images, wip placement

--- src/components/menu/MenuNav.tsx
+++ src/components/menu/MenuNav.tsx
@@ -1,5 +1,10 @@
-import MenuNavLink from './MenuNavLink';
 import { useLocation } from 'react-router-dom';
+import MenuNavLink from './MenuNavLink';
+import workshops from '../../assets/images/workshops.webp';
+import trainings from '../../assets/images/training.webp';
+import contact from '../../assets/images/contact.webp';
+// import upcoming from '../../assets/images/upcoming.webp';
+import home from '../../assets/images/home.webp';
 
 type Props = {
   onNavClose: () => void;
@@ -8,18 +13,70 @@ type Props = {
 const MenuNav = ({ onNavClose }: Props) => {
   const location = useLocation();
 
+  // todo: fix z-indexes or placement as they hide behind the other links when they should be on top
+  // todo: update sizes responsively, too small on large screens
+  // todo: update placement responsively, too close to the link on small screens, and too far on large screens
   const navItems = [
-    { to: '/workshops', label: 'workshops' },
-    { to: '/training', label: 'training' },
-    { to: '/upcoming', label: 'next dates' },
-    { to: '/contact', label: 'contact' },
-    ...(location.pathname !== '/' ? [{ to: '/', label: 'home' }] : []),
+    {
+      to: '/workshops',
+      label: 'workshops',
+      imageSrc: workshops,
+      imageClassName:
+        'left-[-40px] -top-28 rotate-[-2deg] pointer-events-none transition-transform duration-fast ease-standard',
+      altText:
+        '4 brown, white and grey, fluffy ferrets sitting, huddled together, looking at the camera.',
+    },
+    {
+      to: '/training',
+      label: 'training',
+      imageSrc: trainings,
+      imageClassName:
+        'right-[-90px] top-1 pointer-events-none transition-transform duration-fast ease-standard',
+      altText:
+        'Jane Fonda wearing a canary yellow swimsuit cut leotard with capped sleeves and frills, yellow legwarmers with purple cuffs, sitting with one leg up in an aerobics pose.',
+    },
+    // upcoming, // TODO: add upcoming image
+    // {
+    //   to: '/upcoming',
+    //   label: 'next dates',
+    //   imageSrc: '',
+    //   altText: '',
+    // },
+    {
+      to: '/contact',
+      label: 'contact',
+      imageSrc: contact,
+      imageClassName:
+        'left-[-40px] top-9 pointer-events-none transition-transform duration-fast ease-standard',
+      altText:
+        'A multi coloured, marbled crystal ball in pastel shades on a gold star shaped base.',
+    },
+    ...(location.pathname !== '/'
+      ? [
+          {
+            to: '/',
+            label: 'home',
+            imageSrc: home,
+            imageClassName:
+              'right-[-10px] top-1 pointer-events-none transition-transform duration-fast ease-standard rotate-[2deg]',
+            altText:
+              'Pale pink Lego style cottage two story cotrtage house with white picket like railaings.',
+          },
+        ]
+      : []),
   ];
 
   return (
     <nav className="flex flex-col items-center gap-6 py-6 text-lg text-white">
-      {navItems.map(({ to, label }) => (
-        <MenuNavLink key={to} to={to} onClick={onNavClose}>
+      {navItems.map(({ to, label, imageSrc, altText, imageClassName }) => (
+        <MenuNavLink
+          key={to}
+          to={to}
+          imageSrc={imageSrc}
+          imageClassName={imageClassName}
+          altText={altText}
+          onClick={onNavClose}
+        >
           {label}
         </MenuNavLink>
       ))}
--- src/components/menu/MenuNavLink.tsx
+++ src/components/menu/MenuNavLink.tsx
@@ -1,19 +1,43 @@
 import { NavLink } from 'react-router-dom';
+import Image from '../../primitives/image/Image';
 
 type MenuNavLinkProps = {
   to: string;
   children: React.ReactNode;
+  imageSrc?: string;
+  imageClassName?: string;
+  altText?: string;
   onClick: () => void;
 };
 
-const MenuNavLink = ({ to, children, onClick }: MenuNavLinkProps) => (
-  <NavLink
-    to={to}
-    onClick={onClick}
-    className="bg-nav-link hover:bg-nav-link-hover text-nav-link-text hover:text-interactive-hover text-menu-nav-sm md:text-menu-nav-md lg:text-menu-nav-lg duration-fast ease-standard flex w-[80%] max-w-sm items-center justify-center py-2 font-(--font-semibold) transition-colors sm:py-1 lg:py-1"
-  >
-    {children}
-  </NavLink>
+const MenuNavLink = ({
+  to,
+  children,
+  imageSrc,
+  altText,
+  onClick,
+  imageClassName,
+}: MenuNavLinkProps) => (
+  <div className="relative isolate flex w-[80%] max-w-sm items-center justify-center">
+    <NavLink
+      to={to}
+      onClick={onClick}
+      className="bg-nav-link hover:bg-nav-link-hover text-nav-link-text hover:text-interactive-hover text-menu-nav-sm md:text-menu-nav-md lg:text-menu-nav-lg duration-fast ease-standard flex w-full items-center justify-center py-2 font-(--font-semibold) transition-colors sm:py-1 lg:py-1"
+    >
+      {children}
+    </NavLink>
+    {/* // todo: remove the conditional once I have all images, and remoive the coniditonal in props as well */}
+    {imageSrc && altText && (
+      <Image
+        src={imageSrc}
+        width={200}
+        height={200}
+        alt={altText}
+        variant="default"
+        className={`pointer-events-none absolute object-contain ${imageClassName ?? ''}`}
+      />
+    )}
+  </div>
 );
 
 export default MenuNavLink;
