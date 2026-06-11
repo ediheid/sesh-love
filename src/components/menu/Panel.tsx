2175d275bcdb8601857e021fa2f01bdf5c1cda91
Edith Heidmann <eheidmann@gmail.com>
2026-06-11 13:06:22 +0200

    overflow x hidden to prevent side scroll of menu on mobile

--- src/components/menu/Panel.tsx
+++ src/components/menu/Panel.tsx
@@ -3,11 +3,23 @@ type Props = {
   children: React.ReactNode;
 };
 
+// const Panel = ({ navOpen, children }: Props) => {
+//   return (
+//     <aside
+//       className={`bg-primary duration-slow ease-standard absolute top-0 right-0 flex h-full w-full flex-col transition-transform will-change-transform lg:w-1/2 ${
+//         navOpen ? 'translate-x-0' : 'translate-x-[101%]'
+//       }`}
+//     >
+//       {children}
+//     </aside>
+//   );
+// };
+
 const Panel = ({ navOpen, children }: Props) => {
   return (
     <aside
-      className={`bg-primary duration-slow ease-standard absolute top-0 right-0 flex h-full w-full flex-col transition-transform will-change-transform lg:w-1/2 ${
-        navOpen ? 'translate-x-0' : 'translate-x-[101%]'
+      className={`bg-primary duration-slow ease-standard absolute top-0 right-0 flex h-full w-full flex-col overflow-x-hidden transition-transform will-change-transform lg:w-1/2 ${
+        navOpen ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
       {children}
