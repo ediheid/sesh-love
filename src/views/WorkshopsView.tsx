31327990b050c2c5d805c0b7e6e3ff0c383b3633
Edith Heidmann <eheidmann@gmail.com>
2026-06-12 14:16:26 +0200

    view section margins and general layout

--- src/views/WorkshopsView.tsx
+++ src/views/WorkshopsView.tsx
@@ -1,12 +1,7 @@
-import ViewTitle from '../components/typography/ViewTitle';
-
 const WorkshopsView = () => {
   return (
-    <section className="px-4 md:px-6 lg:px-8">
-      {/* <ViewTitle title="workshops" /> */}
-      <div className="flex items-center justify-center">
-        This will be the workshops view
-      </div>
+    <section className="view-section-margins">
+      <div>This will be the workshops view</div>
     </section>
   );
 };
